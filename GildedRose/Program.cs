f61fff15bb9034b1b1b1e7f9c239758cfdbc3a27
Babette Bækgaard <babb@itu.dk>
2021-10-25 17:15:25 +0200

    started implementing switch, single forloop of products, UdateQuality: started impl only upd qual, new method UpdateSellIn

--- GildedRose/Program.cs
+++ GildedRose/Program.cs
@@ -6,61 +6,89 @@ namespace GildedRose
     class Program
     {
         IList<Item> Items;
+
         static void Main(string[] args)
         {
             System.Console.WriteLine("OMGHAI!");
+            const MONTH = 31;
 
             var app = new Program()
-                          {
-                              Items = new List<Item>
-                                          {
-                new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20 },
-                new Item { Name = "Aged Brie", SellIn = 2, Quality = 0 },
-                new Item { Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7 },
-                new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 },
-                new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80 },
-                new Item
-                {
-                    Name = "Backstage passes to a TAFKAL80ETC concert",
-                    SellIn = 15,
-                    Quality = 20
-                },
-                new Item
-                {
-                    Name = "Backstage passes to a TAFKAL80ETC concert",
-                    SellIn = 10,
-                    Quality = 49
-                },
-                new Item
-                {
-                    Name = "Backstage passes to a TAFKAL80ETC concert",
-                    SellIn = 5,
-                    Quality = 49
-                },
-				// this conjured item does not work properly yet
-				new Item { Name = "Conjured Mana Cake", SellIn = 3, Quality = 6 }
-                                          }
-
-                          };
-
-            for (var i = 0; i < 31; i++)
             {
-                Console.WriteLine("-------- day " + i + " --------");
-                Console.WriteLine("name, sellIn, quality");
-                for (var j = 0; j < app.Items.Count; j++)
+                Items = new List<Item>
                 {
-                    Console.WriteLine(app.Items[j].Name + ", " + app.Items[j].SellIn + ", " + app.Items[j].Quality);
+                    new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20 },
+                    new Item { Name = "Aged Brie", SellIn = 2, Quality = 0 },
+                    new Item { Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7 },
+                    new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 },
+                    new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80 },
+
+                    new Item
+                    {
+                        Name = "Backstage passes to a TAFKAL80ETC concert",
+                        SellIn = 15,
+                        Quality = 20
+                    },
+                    new Item
+                    {
+                        Name = "Backstage passes to a TAFKAL80ETC concert",
+                        SellIn = 10,
+                        Quality = 49
+                    },
+                    new Item
+                    {
+                        Name = "Backstage passes to a TAFKAL80ETC concert",
+                        SellIn = 5,
+                        Quality = 49
+                    },
+                    // this conjured item does not work properly yet
+                    new Item { Name = "Conjured Mana Cake", SellIn = 3, Quality = 6 }
                 }
-                Console.WriteLine("");
-                app.UpdateQuality();
-            }
+            };
+
+                for (var i = 0; i < MONTH; i++)
+                {
+                    Console.WriteLine("-------- day " + i + " --------");
+                    Console.WriteLine("name, sellIn, quality");
+
+                    for (var j = 0; j < app.Items.Count; j++)
+                    {
+                        Console.WriteLine(app.Items[j].Name + ", " + app.Items[j].SellIn + ", " + app.Items[j].Quality);
+                        
+                        app.UpdateQuality(Items[j]);
+                        app.UpdateSellIn(Items[j]);
+                    }
+
+                    Console.WriteLine("");
+                }
 
         }
 
-        public void UpdateQuality()
+        public void UpdateSellIn(Item i){
+            i.SellIn--;
+        }
+
+        public void UpdateQuality(Item i)
         {
-            for (var i = 0; i < Items.Count; i++)
-            {
+            var temp = i.Quality;
+            if(temp-- > 5);
+                switch (i.Name)
+                {
+                    case "Aged Brie":
+
+                        break;
+
+                    case "Backstage passes to a TAFKAL80ETC concert":
+
+                        break;
+
+                    case "":
+                        break
+
+                    default:
+                        i.Quality--;
+                }
+
+
                 if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
                 {
                     if (Items[i].Quality > 0)
@@ -131,7 +159,6 @@ namespace GildedRose
                     }
                 }
             }
-        }
 
     }
 
